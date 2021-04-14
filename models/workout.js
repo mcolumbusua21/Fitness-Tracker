00c8e157c5714e3ec22c05d9c7a11e14c59d49bb
Mark Columbus <mcolumbusua21@gmail.com>
2021-04-14 16:53:53 -0700

    setup schema

--- models/workout.js
+++ models/workout.js
@@ -1,15 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: Date.now,
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter an exercise",
+      },
+      duration: {
+        type: Number,
+        required: "Enter an duration",
+      },
+      distance: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
     },
-    exercises: [
-        type: {
-            type: String,
-            
-        }
-    ]
-})
+  ],
+});
+
+
+module.exports.model('Workout', WorkoutSchema)
---
+++ routes/api.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+router.get('/workouts/', async function(req, res){
+
+});
+router.put('/workouts/:id', async function(req, res){
+
+});
+router.get('/workouts', async function(req, res){
+
+});
+router.get('/workouts/range', async function(req, res){
+
+});
+
