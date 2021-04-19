cef070063a3cd0f16e8f25d0a02fa59f00f24855
Mark Columbus <mcolumbusua21@gmail.com>
2021-04-19 13:43:19 -0700

    final touches

--- routes/api.js
+++ routes/api.js
@@ -21,19 +21,21 @@ router.get('/workouts', async function(req, res){
 });
 router.put('/workouts/:id', async function(req, res){
     try{
-        const data = await Workout.updateOne(req.body, {
-            where: {
-                id: req.params.id,
-            },
-        })
+        const data = await Workout.updateOne(
+           { _id: req.params.id },
+           {
+               $push: {
+                   exercises: req.body,
+               }
+           }
+        );
         res.json(data);
     } catch (err) {
         console.log(err);
         res.status(500).send(err);
       }
-    
+    }) ;  
 
-});
 router.post('/workouts', async function(req, res){
     try{
         const data = await Workout.create(req.body);
@@ -46,32 +48,25 @@ router.post('/workouts', async function(req, res){
     }
 });
 router.get('/workouts/range', async function(req, res){
-//     try {
-//         const data = await Workout.aggregate([
-//           {
-//             $addFields: {
-//               totalDuration: {
-//                 $sum: "$exercises.duration",
-//               },
-//             },
-//           },
-//         ])
-//         .sort({'_id': -1})
-//         .limit(7)
-//         console.log(data)
-//         res.json(data);
-//       }catch (err) {
-//           console.log(err);
-//           res.status(500).send(err);
-// }
-Workout.find({}).then(dbWorkout => {
-    console.log("ALL WORKOUTS");
-    console.log(dbWorkout);
+    try {
+        const data = await Workout.aggregate([
+          {
+            $addFields: {
+              totalDuration: {
+                $sum: "$exercises.duration",
+              },
+            },
+          },
+        ])
+        .sort({'_id': -1})
+        .limit(7)
+        console.log(data)
+        res.json(data);
+      }catch (err) {
+          console.log(err);
+          res.status(500).send(err);
+ }
+});
 
-    res.json(dbWorkout);
-}).catch(err => {
-    res.json(err);
-});
-});
 
 module.exports = router;
